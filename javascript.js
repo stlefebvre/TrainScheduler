6f996f20cdd70c3339e1b10467bec8f764e2b44c
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-22 17:06:39 -0400

    Created HTML, JS, and CSS files. Created Firebase database. Linked jQuery, Firebase database, and Bootstrap

---
+++ javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyAWzjhvlaIlAjTHT5keL9USeANBqqSsORA",
+    authDomain: "trainscheduler-91abf.firebaseapp.com",
+    databaseURL: "https://trainscheduler-91abf.firebaseio.com",
+    projectId: "trainscheduler-91abf",
+    storageBucket: "",
+    messagingSenderId: "321952819994"
+    };
+    firebase.initializeApp(config);
+})
